2088beeebb335c40e6b433d5a9242cd13eacbde5
ranaivmi <mickael.ranaivoarisoa@orange.fr>
2018-01-28 18:53:03 +0100

    add tag scripts

---
+++ scripts/tag_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Pulling the images on our registry
+for i in  web i b p s w w1 w2 db
+do
+    docker tag deploycna_$i cloudnativeapp_$i
+    docker tag deploycna__$i 10.11.53.33:5000/cloudnativeapp_$i
+done
+
