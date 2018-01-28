bdc19051d32345e1795fc04907dbd0a958d89792
ranaivmi <mickael.ranaivoarisoa@orange.fr>
2018-01-28 19:07:41 +0100

    correction of tag script

--- scripts/tag_images.sh
+++ scripts/tag_images.sh
@@ -4,6 +4,6 @@
 for i in  web i b p s w w1 w2 db
 do
     docker tag deploycna_$i cloudnativeapp_$i
-    docker tag deploycna__$i 10.11.53.33:5000/cloudnativeapp_$i
+    docker tag deploycna_$i 10.11.53.33:5000/cloudnativeapp_$i
 done
 
